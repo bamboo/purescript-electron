e46fa5cd8c34add1c67d5c1cf47dc3acac41807a
Rodrigo B. de Oliveira <rbo@acm.org>
2016-04-16 09:08:05 -0300

    :arrow_up: purescript@0.8.4

--- test/Main.purs
+++ test/Main.purs
@@ -1,23 +1,16 @@
 module Test.Main where
 
-import Prelude (Unit)
-
-import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Console (CONSOLE)
-
 import Data.Argonaut.Combinators ((~>), (:=))
 import Data.Argonaut.Core (jsonEmptyObject)
 
 import Test.Spec (it, describe)
 import Test.Spec.Assertions (shouldEqual)
 import Test.Spec.Reporter.Console (consoleReporter)
-import Test.Spec.Runner (Process, run)
+import Test.Spec.Runner (run)
 
-import Electron (ELECTRON)
 import Electron.BrowserWindow (BrowserWindowOption(WebPreferences, Height, Width), WebPreference(OverlayScrollbars))
 import Electron.Options (encodeOptions)
 
-main :: forall eff. Eff (console :: CONSOLE, process :: Process, electron :: ELECTRON | eff) Unit
 main = run [consoleReporter] do
   describe "encodeOptions :: BrowserWindowOptions -> Json" do
     it "can encode all options" do
